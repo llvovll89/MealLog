13db49e3fcf98cbe3b460fc7952cd3da794aaec8
llvovll89 <llvovll89@igis.co.kr>
2026-02-11 14:03:42 +0900

    fix: 빌드 실패 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import  { useState } from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import MealRecommendation from './components/MealRecommendation';
